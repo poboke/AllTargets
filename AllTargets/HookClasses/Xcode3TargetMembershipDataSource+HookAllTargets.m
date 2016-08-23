68d7f5e38170bec60989577cf8d38157e56307b8
Francisco Javier Trujillo Mata <francisco.trujillo@betvictor.com>
2016-08-23 15:23:59 +0200

    Selecting Targets depending of the saved information

--- AllTargets/HookClasses/Xcode3TargetMembershipDataSource+HookAllTargets.m
+++ AllTargets/HookClasses/Xcode3TargetMembershipDataSource+HookAllTargets.m
@@ -8,10 +8,15 @@
 
 #import "Xcode3TargetMembershipDataSource+HookAllTargets.h"
 #import "Xcode3TargetWrapper.h"
+#import "AllTargets.h"
+#import "ATSavedData.h"
 
 static NSString * const WrappedTargetsKey = @"wrappedTargets";
 static NSString * const ProductTypeKey = @"productType";
-static NSString * const TestBundleKey = @"PBXXCTestBundleProductType";
+
+static NSString * const ProductAppBundleKey = @"PBXApplicationProductType";
+static NSString * const ProductExtensionBundleKey = @"PBXBundleProductType";
+static NSString * const ProductTestBundleKey = @"PBXXCTestBundleProductType";
 
 @implementation Xcode3TargetMembershipDataSource (HookAllTargets)
 
@@ -29,17 +34,29 @@ static NSString * const TestBundleKey = @"PBXXCTestBundleProductType";
     
     // Run our custom code
     NSMutableArray *wrappedTargets = [self valueForKey:WrappedTargetsKey];
+    ATSavedData *savedData = [[AllTargets sharedPlugin] savedData];
     
     for (Xcode3TargetWrapper *targetWrapper in wrappedTargets) {
+        // Select the targets depending of the ATSavedData configuration
         
-        // Don't select the test targets
-        id pbxTarget = targetWrapper.pbxTarget;
-        id productType = [pbxTarget valueForKey:ProductTypeKey];
-        if ([productType isMemberOfClass:NSClassFromString(TestBundleKey)]) {
-            continue;
+        if (savedData.allTargetSelected) {
+            targetWrapper.selected = YES;
+        } else {
+            id pbxTarget = targetWrapper.pbxTarget;
+            id productType = [pbxTarget valueForKey:ProductTypeKey];
+            NSString *className = NSStringFromClass([productType class]);
+            
+            if ([className isEqualToString:ProductAppBundleKey]) {
+                targetWrapper.selected = savedData.allAppsSelected;
+            } else if ([className isEqualToString:ProductExtensionBundleKey]) {
+                targetWrapper.selected = savedData.allExtensionsSelected;
+            } else if ([className isEqualToString:ProductTestBundleKey]) {
+                targetWrapper.selected = savedData.allTestsSelected;
+            } else {
+                targetWrapper.selected = NO;
+            }
+            
         }
-        
-        targetWrapper.selected = YES;
     }
 }
 
